57076df82985a380127bb836cad232a52fef9023
Tushar <tusharkumargoyal@gmail.com>
2020-04-26 16:27:58 +0530

    Changed app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       Hii!! Finally its working.
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
